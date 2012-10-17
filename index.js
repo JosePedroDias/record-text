45a1b91534636de69db6e20eacc222b205092965
José Pedro Dias <jose.pedro.dias@gmail.com>
2012-10-17 10:25:42 +0100

    about to record

--- index.js
+++ index.js
@@ -1,73 +1,93 @@
 var $ = function(i) {
-	return document.querySelector(i);
-};
-
-
-
-var diff2 = function(o, n) {
-	var d = diff(o, n);
-	//console.log(d);
-	var D = [];
-	var a, b, c, B, C;
-	var idx = 0;
-	for (var i = 0, f = d.length; i < f; ++i) {
-		a = d[i];
-		if ('common' in a) {
-			idx += a.common.length;
-			continue;
-		}
-		b = a.file1;
-		c = a.file2;
-		B = b.join('');
-		C = c.join('');
-
-		D.push([idx, B, C]);
-		idx += c.length - b.length;
-	}
-	return D;
-};
-
-
-
-var updateString = function(d) {
-	var v = t2El.value.split('');
-
-	var kind, idx, b, c, o;
-	for (var i = 0, f = d.length; i < f; ++i) {
-		o   = d[i];
-		idx = o[0];
-		b   = o[1].split('');
-		c   = o[2].split('');
-		v.splice.apply(v, [idx, b.length].concat(c));
-	}
-
-	t2El.value = v.join('');
+    return document.querySelector(i);
 };
 
 
 
 var t1El = $('#t1'),
-	t2El = $('#t2'),
+    t2El = $('#t2'),
     pEl  = $('pre'),
-    o    = '', n;
+    o    = '',
+    n,
+    events,
+    isRecording = false;
+
+
+
+var diff2 = function(o, n) {
+    var d = diff(o, n);
+    //console.log(d);
+    var D = [];
+    var a, b, c, B, C;
+    var idx = 0;
+    for (var i = 0, f = d.length; i < f; ++i) {
+        a = d[i];
+        if ('common' in a) {
+            idx += a.common.length;
+            continue;
+        }
+        b = a.file1;
+        c = a.file2;
+        B = b.join('');
+        C = c.join('');
+
+        D.push([idx, B, C]);
+        idx += c.length - b.length;
+    }
+    return D;
+};
+
+
+
+var updateString = function(d) {
+    var v = t2El.value.split('');
+
+    var kind, idx, b, c, o;
+    for (var i = 0, f = d.length; i < f; ++i) {
+        o   = d[i];
+        idx = o[0];
+        b   = o[1].split('');
+        c   = o[2].split('');
+        v.splice.apply(v, [idx, b.length].concat(c));
+    }
+
+    t2El.value = v.join('');
+};
+
+
+
+
 
 t1El.addEventListener('keydown', function(ev) {
-	if (ev.keyCode !== 9) { return; }
-	ev.preventDefault();
+    if (ev.keyCode !== 9) { return; }
+    ev.preventDefault();
 });
 
-t1El.addEventListener('keyup', function() {
-	n = t1El.value;
-	var d = diff2(o, n);
-	pEl.innerHTML = JSON.stringify(d, null, '\t');
-	o = n;
+var check4Changes = function() {
+    n = t1El.value;
+    var d = diff2(o, n);
+    if (d.length === 0) { return; }
+    pEl.innerHTML = JSON.stringify(d);      // DEBUG
+    o = n;
+    updateString(d);
+};
 
-	updateString(d);
-});
+t1El.addEventListener('keyup', check4Changes);
 
 
 
-// TODO SAVE
+var startRecording = function() {
 
-// TODO REPLAY
+};
 
+
+
+var stopRecording = function() {
+
+};
+
+
+
+var play = function() {
+
+};
